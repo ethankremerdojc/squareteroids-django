7fafb7cf0cf1afb16e51921fe64872cb786525bc
Ethan Kremer <64492390+ethankremerdojc@users.noreply.github.com>
2022-08-07 15:45:04 -0400

    Update settings.py

--- squareteroids/squareteroids/settings.py
+++ squareteroids/squareteroids/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
