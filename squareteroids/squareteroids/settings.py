92c491f60c28ba5867f2ca45627f2f51a8bc1a9d
Ethan Kremer <64492390+ethankremerdojc@users.noreply.github.com>
2022-08-07 15:47:15 -0400

    Update settings.py

--- squareteroids/squareteroids/settings.py
+++ squareteroids/squareteroids/settings.py
@@ -15,9 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -119,9 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
+  os.path.join(BASE_DIR, 'static/'),
 )
 
 # Default primary key field type
