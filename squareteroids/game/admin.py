f78bd58050e903d904271e036dbe59b2f1471a59
Ethan Kremer <64492390+ethankremerdojc@users.noreply.github.com>
2022-08-13 15:19:18 -0400

    Update admin.py

--- squareteroids/game/admin.py
+++ squareteroids/game/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from game.models import Score, Difficulty
-# Register your models here.
 
-admin.site.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('username', 'timestamp', 'difficulty', 'time')
+    readonly_fields = ('timestamp', )
+    fields = (
+        ('username'), 
+        ('hours', 'minutes', 'seconds'), 
+        'difficulty', 
+        ('timestamp')
+    )
+
+admin.site.register(Score, ScoreAdmin)
 admin.site.register(Difficulty)
